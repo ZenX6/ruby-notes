94880379811758a47cb7e45b70bf35a5c0c111dd
Ken <dangken55@gmail.com>
2023-10-25 14:33:37 -0700

    my first ruby practice

---
+++ practice-1.rb
@@ -0,0 +1,20 @@
+#Write a method to_initials that takes in a person's name string and returns a string representing their initials
+
+
+def to_initials(name)
+initials = ""
+
+words = name.split(' ')
+
+words.each do |word|
+    initials += word[0].upcase
+end
+
+return initials
+end
+
+
+
+puts to_initials("Kelvin Bridges")      # => "KB"
+puts to_initials("Michaela Yamamoto")   # => "MY"
+puts to_initials("Mary La Grange")      # => "MLG"
