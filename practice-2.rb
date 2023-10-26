91df92234d6187a7da401ed95d7357200dd25b2c
Ken <dangken55@gmail.com>
2023-10-26 12:58:33 -0700

    added 2d array

--- practice-2.rb
+++ practice-2.rb
@@ -43,3 +43,23 @@ puts
 
 print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
 puts
+
+def combinations(arr)
+  combine = []
+
+  arr.each_with_index do |ele1, idx1|
+    arr.each_with_index do |ele2, idx2|
+      if idx2 > idx1
+        combine << [ele1, ele2]
+      end
+    end
+  end
+
+  return combine
+end
+
+print combinations(["a", "b", "c"]); # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ]
+puts
+
+print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
+puts
