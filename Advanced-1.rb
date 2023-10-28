810aec282b1b1b6de5f6282b013aea954f7f9e91
Ken <dangken55@gmail.com>
2023-10-28 13:21:29 -0700

    updated new question

--- Advanced-1.rb
+++ Advanced-1.rb
@@ -1,5 +1,5 @@
 def map_by_name(arr)
-  arr.map { |hash| hash["name"]}
+  arr.map { |hash| hash["name"] }
 
 
 end
@@ -22,3 +22,31 @@ countries = [
 ]
 print map_by_name(countries) #=> ["Japan", "Hungary", "Kenya"]
 puts
+
+
+def map_by_key(arr, key)
+  arr.map { |hash| hash[key] }
+
+end
+
+locations = [
+  {"city"=>"New York City", "state"=>"New York", "coast"=>"East"},
+  {"city"=>"San Francisco", "state"=>"California", "coast"=>"West"},
+  {"city"=>"Portland", "state"=>"Oregon", "coast"=>"West"},
+]
+
+print map_by_key(locations, "state") #=> ["New York", "California", "Oregon"]
+puts
+print map_by_key(locations, "city") #=> ["New York City", "San Francisco", "Portland"]
+puts
+
+def yell_sentence(sent)
+  words = sent.split(' ')
+
+  new_sent = words.map {|word| word.upcase + '!'}
+
+  new_sent.join(' ')
+
+end
+
+puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
