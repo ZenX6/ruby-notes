c8367d5839f211fa50fad85e0c1ab0ba26370ad3
Ken <dangken55@gmail.com>
2023-10-28 19:03:38 -0700

    isPrime

--- Advanced-1.rb
+++ Advanced-1.rb
@@ -107,3 +107,32 @@ def most_vowels(sentence)
 end
 
 print most_vowels("what a wonderful life") #=> "wonderful"
+
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  elsif num % 2 == 0 || num % 3 == 0
+    return false
+  end
+
+  i = 5
+  while i * i <= num
+    if num % i == 0 || num % (i + 2) == 0
+      return false
+    end
+    i += 6
+  end
+
+  return true
+
+
+end
+
+puts prime?(2)  #=> true
+puts prime?(5)  #=> true
+puts prime?(11) #=> true
+puts prime?(4)  #=> false
+puts prime?(9)  #=> false
+puts prime?(-5) #=> false
