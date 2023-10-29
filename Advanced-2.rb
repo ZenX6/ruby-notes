f9bb6408b3fc0de545877fc243614ec6481e4a5f
Ken <dangken55@gmail.com>
2023-10-29 07:45:29 -0700

    added fibonacci

--- Advanced-2.rb
+++ Advanced-2.rb
@@ -86,3 +86,62 @@ puts perfect_square?(12)  #=> false
 puts perfect_square?(30)  #=> false
 puts perfect_square?(9)   #=> true
 puts perfect_square?(25)  #=> true
+
+
+def triple_sequence(start, length)
+  sequence = [start]
+
+  while sequence.length < length
+    start *= 3
+    sequence << start
+  end
+  sequence
+end
+
+print triple_sequence(2, 4) # => [2, 6, 18, 54]
+puts
+print triple_sequence(4, 5) # => [4, 12, 36, 108, 324]
+puts
+
+def summation_sequence(start, length)
+  sequence = [start]
+
+  while sequence.length < length
+    start = summation(start)
+    sequence << start
+  end
+  sequence
+
+end
+
+def summation(num)
+  (1..num).reduce(0, :+)
+end
+
+
+print summation_sequence(3, 4) # => [3, 6, 21, 231]
+print summation_sequence(5, 3) # => [5, 15, 120]
+
+
+def fibonacci(length)
+  return [] if length == 0
+  return [1] if length == 1
+
+  sequence = [1, 1]
+
+  while sequence.length < length
+    next_num = sequence[-1] + sequence[-2]
+    sequence << next_num
+  end
+  sequence
+
+end
+
+print fibonacci(0) # => []
+puts
+print fibonacci(1) # => [1]
+puts
+print fibonacci(6) # => [1, 1, 2, 3, 5, 8]
+puts
+print fibonacci(8) # => [1, 1, 2, 3, 5, 8, 13, 21]
+puts
