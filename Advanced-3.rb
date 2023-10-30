34122a565013f57c7d13100561ce6a81892d6140
Ken <dangken55@gmail.com>
2023-10-29 17:41:22 -0700

    added doubleLetterCount

--- Advanced-3.rb
+++ Advanced-3.rb
@@ -17,3 +17,42 @@ end
 puts caesar_cipher("apple", 1)    #=> "bqqmf"
 puts caesar_cipher("bootcamp", 2) #=> "dqqvecor"
 puts caesar_cipher("zebra", 4)    #=> "difve"
+
+
+
+def vowel_cipher(string)
+  vowels = 'aeiou'
+  ciphered = ""
+
+  string.each_char do |char|
+    if vowels.include?(char)
+      old_idx = vowels.index(char)
+      new_idx = (old_idx + 1) % 5
+      ciphered += vowels[new_idx]
+    else
+      ciphered += char
+    end
+  end
+
+  ciphered
+
+end
+
+puts vowel_cipher("bootcamp") #=> buutcemp
+puts vowel_cipher("paper cup") #=> pepir cap
+
+def double_letter_count(string)
+  count = 0
+
+  (0...string.length - 1).each do |i|
+    if string[i] == string [i + 1]
+      count += 1
+    end
+  end
+
+  count
+
+end
+
+puts double_letter_count("the jeep rolled down the hill") #=> 3
+puts double_letter_count("bootcamp") #=> 1
